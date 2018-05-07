f8ba707bc216db3a5f3a43b98a745a489d66ddd7
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2018-05-07 12:52:22 -0400

    rrevisualized app

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-//api key for nasa : 2MLiFBTcnqw4cYJF4E7QqEw0PQQqzehW8q3jcR8R
 $(function(){
-    var currentModule;
+
     $('.search').click(function() {
-        currentModule = new SearchModule('.app');
-    });
+
+        $.ajax({
+            url: 'https://api.unsplash.com/photos/?client_id=3fcb101b22812b1e03c942ab3a1135fc005b9a68bc9740c5f922baaff486eea7',
+            method: "GET",
+            success: function(response){
+                console.log(response);
+                $(".container").append(urls.full);
+                
+            }
+        });
+    })
 
 
-
-
-
-
-
-    var url = "https://api.nasa.gov/planetary/apod?api_key=2MLiFBTcnqw4cYJF4E7QqEw0PQQqzehW8q3jcR8R";
-
-    $.ajax({
-        url: url,
-        success: function(result){
-            console.log(result);
-            $('.app').append(result);
-        }
-    });
 
 
 
