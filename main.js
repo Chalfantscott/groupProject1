287aa717703c6552630c1506403fe204042e49dd
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2018-05-08 17:49:58 -0400

    console log unsplash api

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 $(function(){
 
-    $('.search').click(function() {
-
+    $('.btn').click(function(e) {
+        e.preventDefault();
+        console.log(2);
         $.ajax({
             url: 'https://api.unsplash.com/photos/?client_id=3fcb101b22812b1e03c942ab3a1135fc005b9a68bc9740c5f922baaff486eea7',
             method: "GET",
             success: function(response){
                 console.log(response);
-                $(".container").append(urls.full);
-                
+                $('.photos').append(response);
             }
         });
+    })
+    $('.btn').submit(function(e){
+        e.preventDefault();
+        var restaurantSeach = $('.eatStreetSearch').val();
     })
 
 
