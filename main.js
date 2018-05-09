07c3dfe9a97efc7e4e215d91b1077b7b79131496
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2018-05-08 20:35:07 -0400

    unsplash food images in API now

--- main.js
+++ main.js
@@ -4,22 +4,13 @@ $(function(){
         e.preventDefault();
         console.log(2);
         $.ajax({
-            url: 'https://api.unsplash.com/photos/?client_id=3fcb101b22812b1e03c942ab3a1135fc005b9a68bc9740c5f922baaff486eea7',
+            url: 'https://api.unsplash.com/photos/random/?client_id=3fcb101b22812b1e03c942ab3a1135fc005b9a68bc9740c5f922baaff486eea7&query=food&count=10',
             method: "GET",
             success: function(response){
                 console.log(response);
+                console.log(response[0].links.html);
                 $('.photos').append(response);
             }
         });
     })
-    $('.btn').submit(function(e){
-        e.preventDefault();
-        var restaurantSeach = $('.eatStreetSearch').val();
-    })
-
-
-
-
-
-
 });
